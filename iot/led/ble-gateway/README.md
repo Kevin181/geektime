e331a1aa968bd915bf0d08010b5b23ea4b4c2bef
Kevin181 <yuan364205famile@gmail.com>
2022-03-03 17:29:50 +0800

    add ble gateway control light

---
+++ iot/led/ble-gateway/README.md
@@ -0,0 +1,9 @@
+查看raspbian发行版本`lsb_release -a`
+[更新raspbian的源](https://mirrors.tuna.tsinghua.edu.cn/help/raspbian/)
+
+后运行`sudo apt-get update`
+
+
+遇到的问题解决-第八讲精选留言(https://time.geekbang.org/column/article/312691)
+
+网关报错`BLE SCAN error: Failed to execute management command 'pasvend'`
---
+++ iot/led/ble-gateway/mqttdemo.py
@@ -0,0 +1,20 @@
+
+#File: mqttdemo.py 
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    client.subscribe("/geektime/iot")
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+#Still connect to mqtt.eclipse.org
+client.connect("test.mosquitto.org", 1883, 60)
+
+client.loop_forever()
